22ad7cdc91b6f417d9e19efd5674013eecb05eb5
maxca17 <maxcalvert34@gmail.com>
2024-01-03 19:46:05 -0600

    updates 1.11

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
-import Body from './Body.js';
+import Body from './Body';
 import Footer from './Footer';
 
-
 function App() {
+  const [showContactInfo, setShowContactInfo] = useState(false);
+
+  const handleContactClick = () => {
+    setShowContactInfo(true);
+  };
+
+  const handleCloseClick = () => {
+    setShowContactInfo(false);
+  };
+
   return (
     <>
-      <Header/>
-      <Body/>
-      <Footer/>
+      <Header onContactClick={handleContactClick} />
+      <Body />
+      <Footer />
+      {showContactInfo && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={handleCloseClick}>&times;</span>
+            <h4>Contact Information</h4>
+            <p>Email: contact@frostframegaming.com</p>
+            <p>Phone: +1 469-766-6325</p>
+            {/* Additional contact info */}
+          </div>
+        </div>
+      )}
     </>
   );
 }
